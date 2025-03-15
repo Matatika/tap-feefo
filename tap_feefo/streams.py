3f9b60e907571654af31927035845afdcb27395c
Reuben Frankel <rfrankel@matatika.com>
2025-03-15 02:08:33 +0000

    Generate a review ID primary key from MD5-hash of all related feedback IDs

--- tap_feefo/streams.py
+++ tap_feefo/streams.py
@@ -2,13 +2,17 @@
 
 from __future__ import annotations
 
+import hashlib
 from datetime import datetime, timezone
 
+from singer_sdk import typing as th
 from typing_extensions import override
 
 from tap_feefo.client import FeefoStream
 from tap_feefo.schemas.product import ProductRatingObject
 from tap_feefo.schemas.review import ReviewObject
+
+REVIEW_ID_KEY = "_id"
 
 
 class ReviewsStream(FeefoStream):
@@ -17,8 +21,12 @@ class ReviewsStream(FeefoStream):
     name = "reviews"
     path = "/reviews/all"
     records_jsonpath = "$.reviews[*]"
+    primary_keys = (REVIEW_ID_KEY,)
     replication_key = "last_updated_date"
-    schema = ReviewObject.to_dict()
+    schema = th.PropertiesList(
+        *ReviewObject,
+        th.Property(REVIEW_ID_KEY, th.StringType),
+    ).to_dict()
 
     @override
     def get_url_params(self, context, next_page_token):
@@ -36,6 +44,38 @@ class ReviewsStream(FeefoStream):
         params["since_updated_period"] = since_updated_period
 
         return params
+
+    @override
+    def post_process(self, row, context=None):
+        feedback_ids = []
+
+        if service := row.get("service"):
+            feedback_ids.append(service["id"])
+
+        if products := row.get("products"):
+            feedback_ids.extend(p["id"] for p in products)
+
+        if not feedback_ids:
+            self.logger.warning(
+                "Malformed review: no feedback IDs to generate a review "
+                "ID with, skipping"
+            )
+            return None
+
+        self.logger.debug(
+            "Generating review ID from feedback IDs: %s",
+            ", ".join(feedback_ids),
+        )
+
+        review_id = hashlib.md5(
+            "".join(feedback_ids).encode(),
+            usedforsecurity=False,
+        ).hexdigest()
+
+        self.logger.debug("Generated review ID: %s", review_id)
+
+        row[REVIEW_ID_KEY] = review_id
+        return row
 
 
 class ProductRatingsStream(FeefoStream):
