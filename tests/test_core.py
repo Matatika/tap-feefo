06d1da7e0d7ca97172ca03f85624723b6f7b0dcb
Luke <lukemattingly230@gmail.com>
2025-06-10 15:25:33 +0100

    Remove "TODO's and fixed timezone aware date warning

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 from singer_sdk.testing import get_tap_test_class
 
@@ -8,7 +8,7 @@ from tap_feefo.tap import TapFeefo
 
 SAMPLE_CONFIG = {
     "merchant_id": "example-retail-merchant",
-    "start_date": datetime.min.isoformat(),
+    "start_date": datetime.min.replace(tzinfo=timezone.utc).isoformat(),
 }
 
 
@@ -17,6 +17,3 @@ TestTapFeefo = get_tap_test_class(
     tap_class=TapFeefo,
     config=SAMPLE_CONFIG,
 )
-
-
-# TODO: Create additional tests as appropriate for your tap.
